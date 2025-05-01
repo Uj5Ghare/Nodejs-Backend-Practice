8c9c9da305306300efeaabb5a6c961afc3ee5eff
uj5ghare <ujwal5ghare@gmail.com>
2025-05-01 22:31:08 +0530

    (js-practice) learned using ternary operators

---
+++ javascript-practice/ternaryOperator.js
@@ -0,0 +1,24 @@
+function checkEqual(a,b){
+    // if (a === b){
+    //     return true
+    // }
+    // else {
+    //     return false
+    // }
+
+    return (a === b) ? true : false
+}
+console.log(checkEqual(2,1));
+
+
+// if no is positive return positive if it is negetive then return negetive
+function checkNumber(num){
+    return num > 0 ? "positive" : num < 0 ? "negetive" : "Zero"
+}
+
+console.log(checkNumber(0));
+
+
+
+
+
