dac56b3cd83046ec6037d2cab3f944c0dcb72114
uj5ghare <ujwal5ghare@gmail.com>
2025-05-01 22:48:32 +0530

    (js-practice) rest and spread operator

---
+++ javascript-practice/restAndSpreadOperators.js
@@ -0,0 +1,17 @@
+const sum = (function() {
+    return function sum(...args) {
+        return args.reduce((a,b) => a + b, 0);
+    };
+})();
+
+console.log(sum(1,2,3,4));
+
+// spread operator
+const arr1 = ["jan", "feb", "march", "april", "may"]
+let arr2;
+(function() {
+    arr2 = [...arr1];
+    arr1[0] = "patato"
+})()
+
+console.log(arr2);
